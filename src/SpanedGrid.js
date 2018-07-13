05ba9fbdc6e0eac43335a92615f5f81f8f7c37eb
Elcoosp <elcoosp@gmail.com>
2018-07-13 23:28:03 +0200

    doc: Example of Spanned Grid

---
+++ src/SpanedGrid.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { SpanedGrid } from './components/SpanedGrid'
+
+const App = () => (
+  <SpanedGrid debug data={[{ rank: 1 }, { rank: 3 }, { rank: 5 }]}>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>10</div>
+  </SpanedGrid>
+)
+render(<App />, document.getElementById('root'))
